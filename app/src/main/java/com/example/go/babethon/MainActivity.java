a37c993d1560c063086a4b7673622f6e2b148d0e
Nathaniel-Joel Parizi <forexmagnumopus@hushmail.com>
2017-08-12 15:53:40 -0400

    Initial commit

---
+++ app/src/main/java/com/example/go/babethon/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.go.babethon;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+    }
+}
